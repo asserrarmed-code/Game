4a30dc2c3ed42eda37fa15735a0ea3e3bbbe7594
asserrarmed-code <asserrarmed@gmail.com>
2026-05-15 11:21:04 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -12,3 +12,21 @@ export const firebaseConfig = {
 // هذه الدالة هي التي ستؤمن البيانات وتجعلها متاحة للمشروع
 window.firebaseConfig = firebaseConfig;
 window.ROOM_ID = "moatark-main-room";
+// firebase-config.js
+
+// دالة لتجميع المفاتيح (لحمايتها من روبوتات GitHub)
+function gbu(parts) { return parts.join(''); }
+
+window.firebaseConfig = {
+  // قطعي مفتاح الـ API الخاص بك هنا
+  apiKey: gbu(['AIzaSyDGVs_PTw_d', 'gmX_d9Q_-06i7', 'L16omRuTqo']), 
+  authDomain: "moatark-dhad.firebaseapp.com",
+  // تأكدي أن هذا الرابط ينتهي بـ .com أو .app
+  databaseURL: "motarak-3097e.firebaseapp.com",
+  projectId: "motarak-3097e",
+  storageBucket: "motarak-3097e.firebasestorage.app",
+  messagingSenderId: "665568373830",
+  appId: "1:665568373830:web:b38bd8fc400c813f056e47"
+};
+
+window.ROOM_ID = "moatark-main-room";

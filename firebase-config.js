28a4ae59bf757428155c2ba6ea879f3ef7c8f9b3
asserrarmed-code <asserrarmed@gmail.com>
2026-05-15 01:40:22 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyDGVs_PTw_dgmX_d9Q_-06i7L16omRuTqo",
   authDomain: "motarak-3097e.firebaseapp.com",
-  databaseURL: "https://firebase.google.com/docs/web/setup#available-libraries",
+  databaseURL: "https://console.firebase.google.com/u/0/project/motarak-3097e/database/motarak-3097e-default-rtdb/data/~2F",
   projectId: "motarak-3097e",
   storageBucket: "motarak-3097e.firebasestorage.app",
   messagingSenderId: "665568373830",

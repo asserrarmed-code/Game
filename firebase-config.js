4ef26af7d5f05141be5112d399b1d8f0e176012e
asserrarmed-code <asserrarmed@gmail.com>
2026-05-15 12:02:41 +0100

    Update firebase configuration with plain API key
    Replaced obfuscated API key with plain text key and removed comments in Arabic.

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,9 @@
 // firebase-config.js
-// تم إعداد هذا الملف بناءً على بيانات مشروعك الخاصة
-
-function gbu(parts) { 
-    return parts.join(''); 
-}
 
 window.firebaseConfig = {
-  // تشفير بسيط للمفتاح لحمايته من روبوتات GitHub
-  apiKey: gbu(['AIza', 'SyDG', 'Vs_P', 'Tw_d', 'gmX_', 'd9Q_', '-06i', '7L16', 'omRu', 'Tqo']),
-  
+  apiKey: "AIzaSyDGVs_PTw_dgmX_d9Q_-06i7L16omRuTqo",
   authDomain: "motarak-3097e.firebaseapp.com",
-  
-  // هذا هو الرابط الحيوي الذي يربط الأجهزة ببعضها
   databaseURL: "https://motarak-3097e-default-rtdb.europe-west1.firebasedatabase.app",
-  
   projectId: "motarak-3097e",
   storageBucket: "motarak-3097e.firebasestorage.app",
   messagingSenderId: "665568373830",
@@ -21,5 +11,4 @@ window.firebaseConfig = {
   measurementId: "G-5JKTFMSCYN"
 };
 
-// اسم الغرفة للمسابقة
 window.ROOM_ID = "moatark-main-room";
